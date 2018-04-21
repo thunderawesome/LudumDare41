3197973496d50faca9749f576ecd80953183eb8d
Troy Sanders <troyjsanders@gmail.com>
2018-04-21 17:21:48 -0500

    Set up the basics of the character

---
+++ Assets/Scripts/Actions/CharacterAction.cs
@@ -0,0 +1,23 @@
+﻿using InControl;
+
+public class CharacterAction : PlayerActionSet
+{
+    public PlayerAction Left;
+    public PlayerAction Right;
+    public PlayerAction Down;
+    public PlayerAction Up;
+    public PlayerTwoAxisAction Move;
+
+    public PlayerAction Special;
+
+    public CharacterAction()
+    {
+        Left = CreatePlayerAction("Move Left");
+        Right = CreatePlayerAction("Move Right");
+        Down = CreatePlayerAction("Move Down");
+        Up = CreatePlayerAction("Move Up");
+        Move = CreateTwoAxisPlayerAction(Left, Right, Down, Up);
+
+        Special = CreatePlayerAction("Special");
+    }
+}
--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,25 +1,109 @@
-﻿using System.Collections;
+﻿using InControl;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 [RequireComponent(typeof(VoxelAnimation))]
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator))]
 public class Character : MonoBehaviour
 {
     #region Private Variables
     private VoxelAnimation m_voxelAnimation;
+    private CharacterAction m_actions;
+
+    private Rigidbody m_rigidbody;
+    private Animator m_animator;
+
+    private Vector3 m_velocity = Vector3.zero;
+
+    private bool m_isMoving = false;
+    #endregion
+
+    #region Public Variables
+    public float movementSpeed = 5.0f;
     #endregion
 
     #region Unity Methods
     // Use this for initialization
     private void Start()
     {
-        m_voxelAnimation = GetComponent<VoxelAnimation>();
+        InitializeComponents();
+        InitializeInput();
     }
 
     // Update is called once per frame
     private void Update()
     {
+        Input();
+    }
+    #endregion
 
+    #region Private Methods
+    private void InitializeInput()
+    {
+        m_actions = new CharacterAction();
+        m_actions.Left.AddDefaultBinding(InputControlType.DPadLeft);
+        m_actions.Right.AddDefaultBinding(InputControlType.DPadRight);
+        m_actions.Special.AddDefaultBinding(InputControlType.Action1);
+
+        //analog stick
+        m_actions.Left.AddDefaultBinding(InputControlType.LeftStickLeft);
+        m_actions.Right.AddDefaultBinding(InputControlType.LeftStickRight);
+        m_actions.Down.AddDefaultBinding(InputControlType.LeftStickDown);
+        m_actions.Up.AddDefaultBinding(InputControlType.LeftStickUp);
+
+        //keyboard
+        m_actions.Left.AddDefaultBinding(Key.LeftArrow);
+        m_actions.Right.AddDefaultBinding(Key.RightArrow);
+        m_actions.Special.AddDefaultBinding(Key.Z);
+    }
+
+    private void InitializeComponents()
+    {
+        m_voxelAnimation = GetComponent<VoxelAnimation>();
+        m_rigidbody = GetComponent<Rigidbody>();
+        m_animator = GetComponent<Animator>();
+
+        m_rigidbody.constraints = RigidbodyConstraints.FreezeRotationX;
+        m_rigidbody.constraints = RigidbodyConstraints.FreezeRotationZ;
+    }
+
+    private void Input()
+    {
+        Movement();
+        Interact();
+    }
+
+    private void Interact()
+    {
+        if (m_actions.Special.WasPressed)
+        {
+            // Do something
+        }
+    }
+
+    private void Movement()
+    {
+        if (m_actions.Move.IsPressed == true)
+        {
+            m_isMoving = true;
+            CheckIfMoving(m_actions.Move.Value);
+        }
+        else if (m_actions.Move.WasReleased == true)
+        {
+            m_isMoving = false;
+        }
+
+        m_animator.SetBool("IsMoving", m_isMoving);
+        m_animator.SetFloat("Speed", Mathf.Abs(m_velocity.x));
+    }
+
+    private void CheckIfMoving(Vector2 inputValue)
+    {
+        m_velocity = inputValue * movementSpeed;
+        m_rigidbody.velocity = m_velocity;
     }
     #endregion
 }
