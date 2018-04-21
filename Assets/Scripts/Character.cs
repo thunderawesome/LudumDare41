e0fc1f37bdffe609b260dc2ebd7cf9daccb251ca
Troy Sanders <troyjsanders@gmail.com>
2018-04-21 16:49:21 -0500

    Checking in first commit

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(VoxelAnimation))]
+public class Character : MonoBehaviour
+{
+    #region Private Variables
+    private VoxelAnimation m_voxelAnimation;
+    #endregion
+
+    #region Unity Methods
+    // Use this for initialization
+    private void Start()
+    {
+        m_voxelAnimation = GetComponent<VoxelAnimation>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+    #endregion
+}
